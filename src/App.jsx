8d8b7e57fc145d8f10248ba9cdcc2ce5ef63cdcd
Abhishek Sendre <abhisheksendre2169@gmail.com>
2026-05-23 15:36:35 +0530

    updating frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { io } from "socket.io-client";
+import CountUp from "react-countup";
 
 import {
   Shield,
@@ -20,14 +21,24 @@ import {
   Menu,
   X,	
 } from "lucide-react";
-
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from "recharts";
 export default function App() {
 
   // =========================
   // SOCKET
   // =========================
   const [socket, setSocket] = useState(null);
-
   // =========================
   // AUTH
   // =========================
@@ -213,6 +224,53 @@ export default function App() {
 
   ];
 
+// =========================
+// ANALYTICS DATA
+// =========================
+
+const pieData = [
+  {
+    name: "Safe",
+    value: safeRequests,
+  },
+  {
+    name: "Attacks",
+    value: attackRequests,
+  },
+];
+
+const severityData = [
+  {
+    name: "Low",
+    value: logs.filter(
+      (log) => log.severity === "LOW"
+    ).length,
+  },
+  {
+    name: "Medium",
+    value: logs.filter(
+      (log) => log.severity === "MEDIUM"
+    ).length,
+  },
+  {
+    name: "High",
+    value: logs.filter(
+      (log) => log.severity === "HIGH"
+    ).length,
+  },
+  {
+    name: "Critical",
+    value: logs.filter(
+      (log) => log.severity === "CRITICAL"
+    ).length,
+  },
+];
+
+const COLORS = [
+  "#22c55e",
+  "#ef4444",
+];
+
   // =========================
   // AUTH PAGE
   // =========================
@@ -224,8 +282,8 @@ export default function App() {
         className="min-h-screen flex items-center justify-center bg-cover bg-center text-white"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop')",
-        }}
+            "url('https://images.unsplash.com/photo-1510511459019-5dda7724fd87?q=80&w=2070&auto=format&fit=crop')",   
+	}}
       >
 
         <div className="absolute inset-0 bg-black/70 backdrop-blur-sm"></div>
@@ -242,7 +300,7 @@ export default function App() {
             y: 0,
           }}
 
-          className="relative z-10 bg-slate-900/80 border border-cyan-500/30 p-6 sm:p-10 rounded-3xl w-[95%] max-w-[420px] shadow-2xl backdrop-blur-xl"
+          className="transform-gputransform-gpu relative z-10  bg-slate-900/80 border border-cyan-500/30 p-6 sm:p-10 rounded-3xl w-[95%] max-w-[420px] shadow-2xl backdrop-blur-xl"
 	    >
 
           <div className="text-center mb-8">
@@ -329,13 +387,12 @@ export default function App() {
           </div>
 
           <motion.button
-
-            whileHover={{
-              scale: 1.03,
-	      y: -6,
-
-            }}
-
+whileHover={{
+  scale: 1.04,
+  y: -10,
+  rotateX: 4,
+  rotateY: 4,
+}}           
             whileTap={{
               scale: 0.97,
             }}
@@ -390,13 +447,27 @@ export default function App() {
 
 	  style={{
         backgroundImage:
-          "url('https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?q=80&w=2070&auto=format&fit=crop')",
+          "url('https://images.unsplash.com/photo-1510511459019-5dda7724fd87?q=80&w=2070&auto=format&fit=crop')",
       }}
     >
 
-      <div className="absolute inset-0 bg-black/80 backdrop-blur-sm"></div>
+     <div className="absolute inset-0 bg-black/75 backdrop-blur-sm"></div>
 
-{/* MOBILE TOPBAR */}
+<div className="absolute inset-0 opacity-[0.08]">
+
+  <div
+    className="w-full h-full"
+    style={{
+      backgroundImage: `
+        linear-gradient(rgba(0,255,255,0.15) 1px, transparent 1px),
+        linear-gradient(90deg, rgba(0,255,255,0.15) 1px, transparent 1px)
+      `,
+      backgroundSize: "40px 40px",
+    }}
+  />
+
+</div>
+	  {/* MOBILE TOPBAR */}
 
 <div className="lg:hidden fixed top-0 left-0 right-0 z-50 bg-slate-900/90 backdrop-blur-xl border-b border-cyan-500/20 p-4 flex items-center justify-between">
 
@@ -490,7 +561,9 @@ export default function App() {
   setPage(item.name);
   setSidebarOpen(false);
 }}
-              className={`w-full flex items-center gap-3 px-5 py-4 rounded-2xl transition-all duration-300 ${
+           
+
+className={`transfrom-gpu w-full flex items-center gap-3 px-5 py-4 rounded-2xl transition-all duration-300 ${
                 page === item.name
                   ? "bg-cyan-500/20 border border-cyan-400 text-cyan-400 shadow-lg shadow-cyan-500/30"
                   : "bg-slate-800/50 hover:bg-slate-700/60"
@@ -531,9 +604,13 @@ export default function App() {
       </div>
 
       {/* MAIN */}
-    <div className="relative z-10 flex-1 p-4 sm:p-6 lg:p-8 mt-20 lg:mt-0 overflow-auto">  
+   <div className="relative z-10 flex-1 p-4 sm:p-6 lg:p-8 mt-20 lg:mt-0 overflow-auto">
 
+  <div className="absolute top-20 left-20 w-72 h-72 bg-cyan-500/10 rounded-full blur-[120px] animate-pulse"></div>
 
+  <div className="absolute bottom-20 right-20 w-72 h-72 bg-blue-500/10 rounded-full blur-[120px] animate-pulse"></div>
+
+</div>
 	{/* TOPBAR */}
         <div className="flex items-center justify-between mb-10">
 
@@ -561,91 +638,130 @@ export default function App() {
 
         </div>
 
+<motion.div
+  key={page}
+  initial={{
+    opacity: 0,
+    y: 20,
+  }}
+  animate={{
+    opacity: 1,
+    y: 0,
+  }}
+  transition={{
+    duration: 0.4,
+  }}
+>
+
         {/* STATS */}
-      className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-4 gap-4 sm:gap-6 mb-10"      
+    <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-4 gap-4 sm:gap-6 mb-10">
 
-{[
-  {
-    title: "Total Requests",
-    value: totalRequests,
-    color: "cyan",
-    icon: <Activity size={35} />,
-  },
-  {
-    title: "Attack Requests",
-    value: attackRequests,
-    color: "red",
-    icon: <AlertTriangle size={35} />,
-  },
-  {
-    title: "Safe Requests",
-    value: safeRequests,
-    color: "green",
-    icon: <Shield size={35} />,
-  },
-  {
-    title: "Blocked IPs",
-    value: blockedIPs.length,
-    color: "orange",
-    icon: <Ban size={35} />,
-  },
-].map((card, index) => (
+  {[
+    {
+      title: "Total Requests",
+      value: totalRequests,
+      color: "cyan",
+      icon: <Activity size={35} />,
+    },
+    {
+      title: "Attack Requests",
+      value: attackRequests,
+      color: "red",
+      icon: <AlertTriangle size={35} />,
+    },
+    {
+      title: "Safe Requests",
+      value: safeRequests,
+      color: "green",
+      icon: <Shield size={35} />,
+    },
+    {
+      title: "Blocked IPs",
+      value: blockedIPs.length,
+      color: "orange",
+      icon: <Ban size={35} />,
+    },
+  ].map((card, index) => (
 
-  <motion.div
-    key={index}
+    <motion.div
+      key={index}
 
-    whileHover={{
-      scale: 1.03,
-      y: -6,
-    }}
+      whileHover={{
+        scale: 1.03,
+        y: -6,
+      }}
 
-    transition={{
-      type: "spring",
-      stiffness: 180,
-      damping: 18,
-    }}
+      transition={{
+        type: "spring",
+        stiffness: 180,
+        damping: 18,
+      }}
 
-    className={`rounded-3xl p-6 border backdrop-blur-xl shadow-2xl ${
-      card.color === "cyan"
-        ? "bg-cyan-500/10 border-cyan-400/30 shadow-cyan-500/20 hover:shadow-cyan-500/40"
-        : card.color === "red"
-        ? "bg-red-500/10 border-red-400/30 shadow-red-500/20 hover:shadow-red-500/40"
-        : card.color === "green"
-        ? "bg-green-500/10 border-green-400/30 shadow-green-500/20 hover:shadow-green-500/40"
-        : "bg-orange-500/10 border-orange-400/30 shadow-orange-500/20 hover:shadow-orange-500/40"
-    }`}
-  >
+className={`transform-gpu rounded-3xl p-6 border backdrop-blur-xl shadow-2xl ${
+              card.color === "cyan"
+          ? "bg-cyan-500/10 border-cyan-400/30 shadow-cyan-500/20 hover:shadow-cyan-500/40"
+          : card.color === "red"
+          ? "bg-red-500/10 border-red-400/30 shadow-red-500/20 hover:shadow-red-500/40"
+          : card.color === "green"
+          ? "bg-green-500/10 border-green-400/30 shadow-green-500/20 hover:shadow-green-500/40"
+          : "bg-orange-500/10 border-orange-400/30 shadow-orange-500/20 hover:shadow-orange-500/40"
+      }`}
+    >
 
-    <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
 
-      <div>
+        <div>
 
-        <p className="text-slate-300">
-          {card.title}
-        </p>
+          <p className="text-slate-300">
+            {card.title}
+          </p>
 
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mt-5">
-          {card.value}
-        </h1>
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mt-5">
+           <CountUp
+  end={card.value}
+  duration={2}
+/>
+
+	  </h1>
+
+        </div>
+
+        {card.icon}
 
       </div>
 
-      {card.icon}
+    </motion.div>
 
-    </div>
+  ))}
 
-  </motion.div>
+</div>
 
-))}
+<motion.div
+  key={page}
 
-  </div>
+  initial={{
+    opacity: 0,
+    y: 20,
+  }}
 
-        {/* THREAT LOGS */}
+  animate={{
+    opacity: 1,
+    y: 0,
+  }}
+
+  transition={{
+    duration: 0.4,
+  }}
+>
+
+
+       {/* THREAT LOGS */}
+
+<motion.div>
         {page === "logs" && (
 
-          <div className="bg-slate-900/70 border border-cyan-500/20 rounded-3xl p-6 backdrop-blur-xl">
-
-            <h2 className="text-3xl font-bold text-cyan-400 mb-6">
+        <div className="transform-gpu bg-slate-900/70 border border-cyan-400/20 rounded-3xl p-6 backdrop-blur-2xl shadow-[0_0_40px_rgba(6,182,212,0.15)]">
+		<h2 className="text-3xl font-bold text-cyan-400 mb-6">
               Live Threat Logs
             </h2>
 
@@ -727,9 +843,164 @@ export default function App() {
 
         )}
 
+</motion.div>
+
+{page === "analytics" && (
+
+  <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+
+    {/* PIE CHART CARD */}
+    <div className="bg-slate-900/60 border border-cyan-400/20 rounded-3xl p-6 backdrop-blur-2xl shadow-[0_0_40px_rgba(6,182,212,0.15)]">
+
+      <h2 className="text-3xl font-bold text-cyan-400 mb-8">
+        Threat Distribution
+      </h2>
+
+      <div className="w-full h-[420px]">
+
+        <ResponsiveContainer width="100%" height="100%">
+
+          <PieChart>
+
+            <defs>
+
+              <linearGradient id="safeColor" x1="0" y1="0" x2="1" y2="1">
+                <stop offset="0%" stopColor="#22c55e" />
+                <stop offset="100%" stopColor="#16a34a" />
+              </linearGradient>
+
+              <linearGradient id="attackColor" x1="0" y1="0" x2="1" y2="1">
+                <stop offset="0%" stopColor="#ef4444" />
+                <stop offset="100%" stopColor="#dc2626" />
+              </linearGradient>
+
+            </defs>
+
+            <Pie
+              data={pieData}
+              cx="50%"
+              cy="50%"
+              innerRadius={80}
+              outerRadius={140}
+              paddingAngle={8}
+              dataKey="value"
+              label={({ name, percent }) =>
+                `${name} ${(percent * 100).toFixed(0)}%`
+              }
+              labelLine={false}
+            >
+
+              <Cell fill="url(#safeColor)" />
+
+              <Cell fill="url(#attackColor)" />
+
+            </Pie>
+
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "#0f172a",
+                border: "1px solid #06b6d4",
+                borderRadius: "16px",
+                color: "#fff",
+              }}
+            />
+
+          </PieChart>
+
+        </ResponsiveContainer>
+
       </div>
 
     </div>
+
+    {/* ANALYTICS CARD */}
+    <div className="bg-slate-900/60 border border-cyan-400/20 rounded-3xl p-6 backdrop-blur-2xl shadow-[0_0_40px_rgba(6,182,212,0.15)]">
+
+      <h2 className="text-3xl font-bold text-cyan-400 mb-8">
+        Threat Intelligence
+      </h2>
+
+      <div className="space-y-6">
+
+        <div className="bg-slate-800/40 p-5 rounded-2xl">
+          <p className="text-slate-400">
+            Total Threats Detected
+          </p>
+
+          <h1 className="text-5xl font-bold text-red-400 mt-2">
+            {attackRequests}
+          </h1>
+        </div>
+
+        <div className="bg-slate-800/40 p-5 rounded-2xl">
+          <p className="text-slate-400">
+            Safe Traffic
+          </p>
+
+          <h1 className="text-5xl font-bold text-green-400 mt-2">
+            {safeRequests}
+          </h1>
+        </div>
+
+        <div className="bg-slate-800/40 p-5 rounded-2xl">
+          <p className="text-slate-400">
+            Blocked IP Addresses
+          </p>
+
+          <h1 className="text-5xl font-bold text-orange-400 mt-2">
+            {blockedIPs.length}
+          </h1>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+)}
+
+{/* BAR CHART */}
+
+    <div className="bg-slate-900/70 border border-cyan-500/20 rounded-3xl p-6 backdrop-blur-xl">
+
+      <h2 className="text-2xl font-bold text-cyan-400 mb-6">
+        Threat Severity
+      </h2>
+
+      <div className="h-[350px]">
+
+        <ResponsiveContainer width="100%" height="100%">
+
+          <BarChart data={severityData}>
+
+            <CartesianGrid strokeDasharray="3 3" stroke="#334155" />
+
+            <XAxis dataKey="name" stroke="#94a3b8" />
+
+            <YAxis stroke="#94a3b8" />
+
+            <Tooltip />
+
+            <Bar
+              dataKey="value"
+              fill="#06b6d4"
+              radius={[10, 10, 0, 0]}
+            />
+
+          </BarChart>
+
+        </ResponsiveContainer>
+
+      </div>
+
+    </div>
+
+	<motion.div>
+
+</motion.div>	
+</motion.div>
+	</div>
 
   );
 
